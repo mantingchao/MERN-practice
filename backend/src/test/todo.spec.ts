074bed578df852eab74ab76d78b38b0d771785b8
Manting <manting417@gmail.com>
2021-07-09 10:03:45 +0000

    update test/todo

--- backend/src/test/todo.spec.ts
+++ backend/src/test/todo.spec.ts
@@ -34,4 +34,42 @@ describe('Form test', () => {
     })    
 
     // TODO: Add some test cases like CRUD, i.e. get, post, update, delete
+
+    it('should get todo item after input', async () => {
+        const response = await server.inject({
+            method: 'POST',
+            url: '/api/todos',
+            payload: {
+                name: 'clean my desk',
+                description: 'Should clean my desk before dinner.',
+                status: false
+            }
+        })
+
+        expect(response.statusCode).toBe(201)
+        const res: { todo: ITodo } = JSON.parse(response.body)
+        expect(res.todo.name).toBe('clean my desk')
+        expect(res.todo.description).toBe('Should clean my desk before dinner.')
+
+
+            
+        //get
+        const getResponse = await server.inject({
+            method: 'GET',
+            url: '/api/todos'
+        })
+
+        expect(getResponse.statusCode).toBe(200)
+        const getRes: { todos: Array<ITodo> } = JSON.parse(getResponse.body)
+        console.log(`post Todo: ${getResponse.body}`)
+        expect(getRes.todos.length).toBe(1)
+        expect(getRes.todos[0].name).toBe('clean my desk')
+        expect(getRes.todos[0].description).toBe('Should clean my desk before dinner.')
+        expect(getRes.todos[0].status).toBe(false)
+
+        //update
+        
+        
+        
+    })
 })
--- frontend/src/test/App.test.tsx
+++ frontend/src/test/App.test.tsx
@@ -3,3 +3,8 @@ import { render, screen } from '@testing-library/react'
 import App from '../App'
 
 // TODO: Add some simple test cases, e.g. checking components are rendered successfully
+test('should show the title', () => {
+    render(<App />)
+    const todoTitle = screen.getAllByText('My Todos')
+    expect(todoTitle).toBeInTheDocument()
+})
